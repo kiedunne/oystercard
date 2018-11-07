b1846d574428828a3681091b5d6d26fd1d92d0a8
kiedunne <kie.dunne@gmail.com>
2018-11-07 15:36:34 +0000

    Challenge - 14 - complete

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,56 +2,33 @@ require_relative 'station'
 require_relative 'oystercard'
 
 class Journey
-  attr_reader :in_transit, :journey, :fare
+  attr_reader :journey, :fare
   MIN_FARE = 1
   MAX_FARE = 6
 
   def initialize
-    @in_transit = false
     @journey = {}
     @fare = 0
   end
 
   def enter_station(station=nil)
-    @in_transit = true
     @journey[:enter] = station
   end
 
   def exit_station(station=nil)
-    @in_transit = false
     @journey[:exit] = station
   end
 
   def in_transit?
-    @in_transit
+    !@journey[:enter].nil? && @journey[:exit].nil?
+  end
+
+  def complete?
+    !@journey[:enter].nil? && !@journey[:exit].nil?
   end
 
   def fare
-    if @journey.length.even?
-      MIN_FARE
-    else
-      MAX_FARE
-    end
+    complete? ? MIN_FARE : MAX_FARE
   end
 
-
-
-
 end
-
-
-# class Journey
-#   attr_reader :entry_station, :exit_station
-#
-#   PENALTY_FARE = 6
-#
-#   def initialize(entry_station: nil)
-#     @entry_station = entry_station
-#     @complete = false
-#   end
-#
-#   def exit(station=nil)
-#     @exit_station = station
-#     @complete = true
-#     self
-#   end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -7,53 +7,51 @@ describe Journey do
 let(:min_fare) { Journey::MIN_FARE }
 let(:max_fare) { Journey::MAX_FARE }
 
-let(:station1) { double :station, name: 'Angel', zone: 1 }
-let(:station2) { double :station, name: 'Bank', zone: 1 }
-let(:station3) { double :station, name: 'Epping', zone: 3 }
+let(:station_angel) { double :station_angel, name: 'Angel', zone: 1 }
+let(:station_bank) { double :station_bank, name: 'Bank', zone: 1 }
+# let(:station_epping) { double :station_epping, name: 'Epping', zone: 3 }
 
   it "Stores entry and exit stations" do
-    subject.enter_station(station1)
-    subject.exit_station(station2)
-    trip = { :enter => station1, :exit => station2 }
+    subject.enter_station(station_angel)
+    subject.exit_station(station_bank)
+    trip = { :enter => station_angel, :exit => station_bank }
     expect(subject.journey).to eq trip
   end
 
     describe ' in_transit?' do
       it 'User is in transit when they tap in' do
-        subject.enter_station(station1)
+        subject.enter_station(station_angel)
         expect(subject).to be_in_transit
         end
 
       it 'User is not in transit when they complete journey' do
-        subject.enter_station(station1)
-        subject.exit_station(station2)
+        subject.enter_station(station_angel)
+        subject.exit_station(station_bank)
         expect(subject).to_not be_in_transit
         end
 
       it 'User is not in transit when they just tap out' do
-        subject.exit_station(station2)
+        subject.exit_station(station_bank)
         expect(subject).to_not be_in_transit
         end
     end
 
     describe ' fare' do
       it 'Returns minimum fare when there is an exit and entry station' do
-        subject.enter_station(station1)
-        subject.exit_station(station2)
+        subject.enter_station(station_angel)
+        subject.exit_station(station_bank)
         expect(subject.fare).to eq min_fare
       end
 
       it 'Returns maximum fare when there is only an exit station' do
-        subject.exit_station(station2)
+        subject.exit_station(station_bank)
         expect(subject.fare).to eq max_fare
       end
 
       it 'Returns maximum fare when there is only an entry station' do
-        subject.exit_station(station1)
+        subject.exit_station(station_angel)
         expect(subject.fare).to eq max_fare
       end
     end
-
-
 
 end
